2ae0e206b08b5257100c67ed392c2aad036ed5ce
👻👽🤖🧙‍♂️🫠 <sprain6628@gmail.com>
2025-05-03 00:25:32 +0900

    렌더러 컴포저블 제네릭화

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,26 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   app: {
     head: {
       // 기본 제목 (titleTemplate이 설정되면 이것은 %s가 없을 때의 기본값)
-      title: '',
+      title: "",
       // 제목 템플릿 (%s는 useHead에서 설정한 title로 대체됨)
-      titleTemplate: '%s | Charts',
+      titleTemplate: "%s | Charts",
       // 다른 전역 head 설정...
     },
     // GitHub Pages 저장소 이름으로 baseURL 설정
-    baseURL: process.env.NODE_ENV === 'production' ? '/charts/' : '/',
+    baseURL: process.env.NODE_ENV === "production" ? "/charts/" : "/",
     // buildAssetsDir: '/assets/' // GitHub Pages에서 정적 에셋 경로 문제 방지
   },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  }
+    plugins: [tailwindcss()],
+  },
+  build: {
+    transpile: ["gifenc"],
+  },
 })
